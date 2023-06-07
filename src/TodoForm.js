7e208034c902e2971fd832dfd913d1c3e7d06831
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-06-07 23:08:47 +0800

    initial commit

---
+++ src/TodoForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+const TodoForm = ({ saveTodo }) => {
+  return (
+    <form>
+      <TextField
+        variant="outlined"
+        placeholder="Add todo"
+        margin="normal"
+      />
+   </form>
+  );
+};
+
+
+export default TodoForm;
